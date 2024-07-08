62daa97261aa933d25f6202ea92cb7bb8d5bc06e
Petrov Kirill <ottokirik@gmail.com>
2024-07-08 15:00:43 +0300

    feat: aspect ratio

--- src/hooks/use-canvas.ts
+++ src/hooks/use-canvas.ts
@@ -6,6 +6,20 @@ export default function useCanvas() {
 
 	let canvasCtx: CanvasRenderingContext2D | null = null;
 
+	function calculateAspectRatio(
+		srcWidth: number,
+		srcHeight: number,
+		maxWidth: number,
+		maxHeight: number,
+	) {
+		const ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+
+		return {
+			width: srcWidth * ratio,
+			height: srcHeight * ratio,
+		};
+	}
+
 	function loadImage(url: string) {
 		if (!canvasEl.value) return;
 
@@ -19,7 +33,23 @@ export default function useCanvas() {
 	function drawOriginalImage() {
 		if (!canvasCtx || !canvasEl.value) return;
 
-		canvasCtx.drawImage(imgEl, 0, 0);
+		const newImageDimension = calculateAspectRatio(
+			imgEl.naturalWidth,
+			imgEl.naturalHeight,
+			448,
+			448,
+		);
+
+		canvasEl.value.width = newImageDimension.width;
+		canvasEl.value.height = newImageDimension.height;
+
+		canvasCtx.drawImage(
+			imgEl,
+			0,
+			0,
+			newImageDimension.width,
+			newImageDimension.height,
+		);
 	}
 
 	return { canvasEl, loadImage };
