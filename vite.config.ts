b5c350193d8202ea398ec6953966c3ca05532872
Petrov Kirill <ottokirik@gmail.com>
2024-07-08 12:22:00 +0300

    feat: bg component

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+import Unfonts from 'unplugin-fonts/vite';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+	plugins: [
+		vue(),
+		Unfonts({
+			google: {
+				families: ['Quicksand'],
+			},
+		}),
+		Components(),
+	],
+});
